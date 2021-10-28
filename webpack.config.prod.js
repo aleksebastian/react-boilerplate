6f90b68a655db933aaee183702922c42ad871d63
Alek <aleksebastian@gmail.com>
2021-10-28 09:04:04 -0500

    Added webpack compression configuration for build

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,3 +1,6 @@
+const CompressionPlugin = require("compression-webpack-plugin");
+const zlib = require("zlib");
+
 module.exports = {
   mode: "production",
   entry: __dirname + "/client/src/index.js",
@@ -24,4 +27,26 @@ module.exports = {
     path: __dirname + "/client/dist",
   },
   devtool: "source-map",
+  plugins: [
+    new CompressionPlugin({
+      filename: "[path][base].gz",
+      algorithm: "gzip",
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 10240,
+      minRatio: 0.8,
+    }),
+    new CompressionPlugin({
+      filename: "[path][base].br",
+      algorithm: "brotliCompress",
+      test: /\.(js|css|html|svg)$/,
+      compressionOptions: {
+        params: {
+          [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
+        },
+      },
+      threshold: 10240,
+      minRatio: 0.8,
+      deleteOriginalAssets: false,
+    }),
+  ],
 };
